a95cd34f1379de6e7ce51289710dbe16d37ad67f
0tuedon <tuedonmofe@gmail.com>
2022-06-14 13:54:38 +0100

    Chore:Setyp repo for tradaxs.com

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        accent: "#1B3593"
+      }
+    },
+  },
+  plugins: [],
+}
