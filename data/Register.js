4b094d52f4fca324f3c1b69019c57abcdcbaeb8f
MickelsonKlaus <klausmickelson52@email.com>
2022-06-16 06:05:34 +0100

    finished login & register pages

---
+++ data/Register.js
@@ -0,0 +1,37 @@
+export const registerStructure = [
+    {
+        as: "label",
+        label: "Full name",
+        name: "name",
+        type: "text",
+        placeholder: "Enter full name",
+    },
+    {
+        as: "label",
+        label: "Username",
+        name: "username",
+        type: "text",
+        placeholder: "Enter a username",
+    },
+    {
+        as: "label",
+        label: "Email",
+        name: "email",
+        type: "email",
+        placeholder: "Email address",
+    },
+    {
+        as: "label",
+        label: "Phone number",
+        name: "phone",
+        type: "tel",
+        placeholder: "Enter your phone number",
+    },
+    {
+        as: "label",
+        label: "Password",
+        name: "password",
+        type: "password",
+        placeholder: "Password",
+    },
+];
