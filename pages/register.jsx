831554a3f76196fbe9f49c399567942a9b3a7600
MickelsonKlaus <klausmickelson52@email.com>
2022-06-16 06:10:43 +0100

    finished login & register pages

--- pages/register.jsx
+++ pages/register.jsx
@@ -22,7 +22,11 @@ const Register = () => {
     <section className="grid max-w-full min-h-screen p-5 md:px-10 bg-bgray place-items-center">
       <div className="w-full h-auto max-w-md p-5 pb-10 bg-white rounded-md">
         <span className="grid mx-auto mb-5 w-fit place-items-center">
-          <LogoAccent className="w-10 h-10" />
+          <Link href="/" passHref>
+            <a>
+              <LogoAccent className="w-10 h-10" />
+            </a>
+          </Link>
           <h3 className="my-0 text-sm font-medium md:text-base">Tradaxs</h3>
         </span>
         <h1 className="text-base font-medium text-center md:text-lg">
