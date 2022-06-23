ba2be4914288debeeb9e6c9dc3946c0876c2913d
MickelsonKlaus <klausmickelson52@email.com>
2022-06-23 09:00:17 +0100

    landing page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Boundaries from "../components/Home/Sections/Boundaries";
 import LandingFooter from "../components/Landing/Footer/LandingFooter";
 import HeroSection from "../components/Landing/Hero/HeroSection";
 import StartMining from "../components/Landing/Mining/StartMining";
@@ -5,18 +6,19 @@ import Stats from "../components/Landing/Statistics/Stats";
 import WhyYouShould from "../components/Landing/Statistics/WhyYouShould";
 import Trade from "../components/Landing/Trade/Trade";
 import PublicLayout from "../components/Layouts/PublicLayout";
+import Features from "../components/Landing/Features";
 
 export default function Home() {
   return (
-    <>
-      <PublicLayout>
-        <HeroSection />
-        <Stats/>
-        <WhyYouShould/>
-        <Trade/>
-        <StartMining/>
-        <LandingFooter/>
-      </PublicLayout>
-    </>
+    <PublicLayout>
+      <HeroSection />
+      <Stats />
+      <WhyYouShould />
+      <Boundaries />
+      <Features />
+      {/* <Trade /> */}
+      <StartMining />
+      <LandingFooter />
+    </PublicLayout>
   );
 }
