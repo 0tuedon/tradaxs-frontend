b58b988d3098f8676615235c2ac5e710432565db
MickelsonKlaus <klausmickelson52@email.com>
2022-07-09 22:06:16 +0100

    Fix: spacing between transaction card content
    Changed from "Justify-between" to "Justify-start" to make it more appealing

--- components/Dashboard/Transactions.jsx
+++ components/Dashboard/Transactions.jsx
@@ -5,13 +5,13 @@ import { MdSwapHoriz } from "react-icons/md";
 
 const Transactions = () => {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mb-10 font-medium text-white gap-x-2 gap-y-3 md:gap-5 place-items-start justify-content-start max-w-3xl">
-      <div className="flex items-center justify-between px-2 sm:px-5 py-3 sm:p-3 space-x-3 rounded-md w-full max-w-full h-full bg-landingBlue">
+    <div className="grid max-w-3xl grid-cols-2 mb-10 font-medium text-white md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-3 md:gap-5 place-items-start justify-content-start">
+      <div className="flex items-center justify-start w-full h-full max-w-full px-2 py-3 space-x-3 rounded-md sm:p-3 bg-landingBlue">
         <span>
           <FaWallet className="w-8 h-8" />
         </span>
         <span>
-          <h2 className="text-xs sm:text-base md:text-lg">
+          <h2 className="text-sm sm:text-base md:text-lg">
             {new Intl.NumberFormat("en-US", {
               style: "currency",
               currency: "USD",
@@ -22,12 +22,12 @@ const Transactions = () => {
           </p>
         </span>
       </div>
-      <div className="flex items-center justify-between px-2 sm:px-5 py-3 sm:p-3 space-x-3 rounded-md bg-landingBlue w-full h-full max-w-full">
+      <div className="flex items-center justify-start w-full h-full max-w-full px-2 py-3 space-x-3 rounded-md sm:p-3 bg-landingBlue">
         <span>
           <FaMoneyBill className="w-8 h-8" />
         </span>
         <span>
-          <h2 className="text-xs sm:text-base md:text-lg">
+          <h2 className="text-sm sm:text-base md:text-lg">
             {new Intl.NumberFormat("en-US", {
               style: "currency",
               currency: "USD",
@@ -39,12 +39,12 @@ const Transactions = () => {
         </span>
       </div>
       <Link href="/dashboard/transactions" passHref>
-        <a className="flex items-center justify-between px-2 sm:px-5 py-3 sm:p-3 space-x-3 rounded-md bg-landingBlue h-full w-full max-w-full">
+        <a className="flex items-center justify-start w-full h-full max-w-full px-2 py-3 space-x-3 rounded-md sm:p-3 bg-landingBlue">
           <span>
             <MdSwapHoriz className="w-8 h-8" />
           </span>
           <span>
-            <h2 className="text-xs sm:text-base md:text-lg">50</h2>
+            <h2 className="text-sm sm:text-base md:text-lg">50</h2>
             <p className="text-[0.625rem] sm:text-xs md:text-sm text-black/50">
               Transactions
             </p>
