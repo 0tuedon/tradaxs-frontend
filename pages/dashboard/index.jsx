09cca9ab6da63e084d80ffba6fd7395dbf703e0d
MickelsonKlaus <klausmickelson52@email.com>
2022-07-19 21:07:02 +0100

    chore(dep): changed chart dependency

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import LineChart from "../../components/Dashboard/Chart";
+//import LineChart from "../../components/Dashboard/Chart";
 import History from "../../components/Dashboard/History";
 import Trends from "../../components/Dashboard/Trends";
 import Wallets from "../../components/Dashboard/Wallets";
 import AuthLayout from "../../components/Layouts/AuthLayout";
+//import TradeViewChart from "react-crypto-chart";
+import dynamic from "next/dynamic";
+
+const CryptoChart = dynamic(() => import("react-crypto-chart"), {
+  ssr: false,
+});
 
 const Profile = () => {
   useEffect(() => {
@@ -21,10 +27,49 @@ const Profile = () => {
       <ToastContainer autoClose={1000} />
       <section className="w-full text-white md:pt-4">
         <Wallets />
-        <div className="w-full max-w-full">
-          <LineChart />
+        <div className="w-full max-w-full my-10 h-80 lg:h-96">
+          {/* <LineChart /> */}
+          <CryptoChart
+            pair="BTCBUSD"
+            chartLayout={{
+              layout: {
+                backgroundColor: "#0C4DAE",
+                textColor: "#ffffff",
+              },
+              grid: {
+                vertLines: {
+                  color: "transparent",
+                  // style: LineStyle.SparseDotted,
+                },
+                horzLines: {
+                  color: "transparent",
+                  // style: LineStyle.SparseDotted,
+                },
+              },
+              // crosshair: {
+              //   mode: CrosshairMode.Normal,
+              // },
+              priceScale: {
+                borderColor: "#485c7b",
+              },
+              timeScale: {
+                borderColor: "#485c7b",
+                timeVisible: true,
+                secondsVisible: false,
+              },
+            }}
+            containerStyle={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: "5px",
+              overflow: "hidden",
+            }}
+          />
         </div>
-        <div className="flex flex-col mt-5 mb-10 lg:flex-row space-y-10 lg:space-y-0 lg:space-x-10">
+        <div className="flex flex-col mt-5 mb-10 space-y-10 lg:flex-row lg:space-y-0 lg:space-x-10">
           <Trends />
           <History />
         </div>
